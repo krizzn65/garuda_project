3465eff220a6a7f5dbe9afe49d489111e0ea18ea
krizzn65 <krisnadewa6565@gmail.com>
2025-12-14 02:23:52 +0700

    jujur ngerjain sambil ngantuk

--- storage/framework/views/e2cb5da9e98c4a1f5b1d02b227ad6374.php
+++ storage/framework/views/e2cb5da9e98c4a1f5b1d02b227ad6374.php
@@ -153,7 +153,7 @@
                                 <p class="text-sm text-garuda-grey">Transit <?php echo e($flight->segments->count() - 2); ?>x</p>
                             </div>
                             <p class="min-w-[120px] font-semibold text-garuda-green text-center"><?php echo e('Rp. ' . number_format($flight->classes->first()->price, 0, ',', '.')); ?></p>
-                            <a href="choose-tiers.html"
+                            <a href="<?php echo e(route('flight.show', $flight->flight_number)); ?>"
                                 class="rounded-full py-3 px-5 text-center bg-garuda-blue hover:shadow-[0px_14px_30px_0px_#0068FF66] transition-all duration-300">
                                 <span class="font-semibold text-white">Choose</span>
                             </a>
@@ -231,7 +231,7 @@
                             <?php echo e('Rp. ' . number_format($flight->classes->first()->price, 0, ',', '.')); ?>
 
                         </p>
-                        <a href="choose-tiers.html"
+                        <a href="<?php echo e(route('flight.show', $flight->flight_number)); ?>"
                             class="rounded-full py-3 px-5 text-center bg-garuda-blue hover:shadow-[0px_14px_30px_0px_#0068FF66] transition-all duration-300">
                             <span class="font-semibold text-white">Choose</span>
                         </a>
