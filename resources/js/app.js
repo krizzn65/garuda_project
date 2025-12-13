ff23062eb755cabc5ec01f83ac28a439a284e3f4
krizzn65 <krisnadewa6565@gmail.com>
2025-12-13 16:19:36 +0700

    perbaiki tampilan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+// Import custom JavaScript files
+import '../../../public/assets/js/index.js';
+import '../../../public/assets/js/accodion.js';
+import '../../../public/assets/js/chose-seat.js';
+import '../../../public/assets/js/date-of-birth.js';
