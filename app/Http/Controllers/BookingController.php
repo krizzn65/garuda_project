a8fc9e02bc4ddd07564048f35dd44dd333ebd457
krizzn65 <krisnadewa6565@gmail.com>
2025-12-14 01:58:23 +0700

    perbaiki check booking

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    public function checkBooking(){
+        return view('pages.booking.check-booking');
+    }
+}
