abc194548963ad2717e4448d5702db833017861e
krizzn65 <krisnadewa6565@gmail.com>
2025-12-16 17:58:49 +0700

    nambahi midtrans

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -55,6 +55,28 @@ class BookingController extends Controller
         return view('pages.booking.checkout', compact('flight', 'tier'));
     }
 
+    public function payment(Request $request){
+        $this->transactionRepository->saveTransactionDataToSession($request->all());
+
+        $transaction = $this->transactionRepository->saveTransaction($this->transactionRepository->getTransactionDataFromSession());
+
+        \Midtrans\Config::$serverKey = config('midtrans.serverKey');
+        \Midtrans\Config::$isProduction = config('midtrans.isProduction');
+        \Midtrans\Config::$isSanitized = config('midtrans.isSanitized');
+        \Midtrans\Config::$is3ds = config('midtrans.is3ds');
+
+        $params = [
+            'transaction_details' => [
+                'order_id' => $transaction->code,
+                'gross_amount' => $transaction->grandtotal,
+            ],
+        ];
+
+        $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+
+        return redirect($paymentUrl);
+    }
+
     public function checkBooking(){
         return view('pages.booking.check-booking');
     }
