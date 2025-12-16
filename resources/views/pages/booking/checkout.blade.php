33c119f6f8a08863469d5d9869b7a6686559d96a
krizzn65 <krisnadewa6565@gmail.com>
2025-12-16 14:11:17 +0700

    otak atik promo code

--- resources/views/pages/booking/checkout.blade.php
+++ resources/views/pages/booking/checkout.blade.php
@@ -273,38 +273,7 @@
                     </div>
                 </div>
                 @endforeach
-                <div id="Promo" class="flex flex-col rounded-[20px] p-5 gap-5 bg-white overflow-hidden">
-                    <h2 class="font-bold text-xl leading-[30px]">Apply Promo</h2>
-                    <label class="flex flex-col gap-[10px]">
-                        <p class="font-semibold">Your Promo Code</p>
-                        <div class="flex items-center flex-nowrap gap-[10px]">
-                            <div
-                                class="flex items-center rounded-full border border-garuda-black py-3 px-5 gap-[10px] focus-within:border-[#0068FF] transition-all duration-300">
-                                <img src="assets/images/icons/receipt-discount-black.svg" class="w-5 flex shrink-0"
-                                    alt="icon">
-                                <input type="text" name="" id=""
-                                    class="appearance-none outline-none w-full font-semibold placeholder:font-normal"
-                                    placeholder="Input promo code">
-                                <img src="assets/images/icons/verify.svg" class="w-5 flex shrink-0" alt="icon">
-                            </div>
-                            <span class="font-semibold text-garuda-green text-nowrap">Kode promo tersedia</span>
-                        </div>
-                    </label>
-                    <label class="flex flex-col gap-[10px]">
-                        <p class="font-semibold">Your Promo Code</p>
-                        <div class="flex items-center flex-nowrap gap-[10px]">
-                            <div
-                                class="flex items-center rounded-full border border-garuda-black py-3 px-5 gap-[10px] focus-within:border-[#0068FF] transition-all duration-300">
-                                <img src="assets/images/icons/receipt-discount-black.svg" class="w-5 flex shrink-0"
-                                    alt="icon">
-                                <input type="text" name="" id=""
-                                    class="appearance-none outline-none w-full font-semibold placeholder:font-normal"
-                                    placeholder="Input promo code">
-                            </div>
-                            <span class="font-semibold text-garuda-red text-nowrap">Kode promo tidak tersedia</span>
-                        </div>
-                    </label>
-                </div>
+                @livewire('check-promo-code')
                 <div id="Payment-Method" class="flex flex-col rounded-[20px] p-5 gap-5 bg-white overflow-hidden">
                     <h2 class="font-bold text-xl leading-[30px]">Payment Method</h2>
                     <div class="flex flex-col gap-[10px]">
