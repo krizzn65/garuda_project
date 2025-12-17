13e39c4fdde782624b84eddf1e5dcf9ab2e75bb1
krizzn65 <krisnadewa6565@gmail.com>
2025-12-17 23:28:24 +0700

    ngantuk wesan

--- resources/views/pages/booking/succes.blade.php
+++ resources/views/pages/booking/succes.blade.php
@@ -21,50 +21,76 @@
                         <div class="flex justify-between">
                             <div>
                                 <p class="text-sm text-garuda-grey">Departure</p>
-                                <p class="font-semibold text-lg">Jakarta (CGK)</p>
+                                <p class="font-semibold text-lg">{{ $transaction->flight->segments->first()->airport->name}} 
+                                    {{ $transaction->flight->segments->first()->airport->iata_code}}</p>
                             </div>
                             <div class="text-end">
                                 <p class="text-sm text-garuda-grey">Arrival</p>
-                                <p class="font-semibold text-lg">Tokyo (HND)</p>
+                                <p class="font-semibold text-lg">{{ $transaction->flight->segments->last()->airport->name}} 
+                                    {{ $transaction->flight->segments->last()->airport->iata_code}}</p>
                             </div>
                         </div>
                         <div class="flex justify-between">
                             <div>
                                 <p class="text-sm text-garuda-grey">Date</p>
-                                <p class="font-semibold text-lg">15 Sep 2024</p>
+                                <p class="font-semibold text-lg">{{$transaction->flight->segments->first()->time->format('d F y')}}4</p>
                             </div>
                             <div class="text-end">
                                 <p class="text-sm text-garuda-grey">Quantity</p>
-                                <p class="font-semibold text-lg">3 people</p>
+                                <p class="font-semibold text-lg">{{$transaction->passengers->count()}} people</p>
                             </div>
                         </div>
                         <div class="flex flex-col rounded-[20px] border border-[#E8EFF7] p-5 gap-5">
                             <div class="flex flex-col gap-4">
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-center gap-[10px]">
-                                        <img src="assets/images/logos/ana.svg" class="h-[100px] flex shrink-0"
-                                            alt="logo">
-                                    </div>
-                                    <a href="#"
-                                        class="flex items-center rounded-[50px] py-3 px-5 gap-[10px] w-fit bg-garuda-black">
-                                        <p class="font-semibold text-white">Details</p>
-                                    </a>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center gap-[10px]">
+                                <img src="{{asset('storage/'.$transaction->flight->airline->logo)}}" class="w-[60px] h-[60px] flex shrink-0" alt="logo">
+                                <div>
+                                    <p class="font-semibold">{{$transaction->flight->airline->name}}</p>
+                                    <p class="text-sm text-garuda-grey mt-[2px]">{{$transaction->flight->segments->first()->time->format('H:i') . ' - ' . $transaction->flight->segments->last()->time->format('H:i')}}</p>
                                 </div>
-                                <div class="flex items-center justify-between">
-                                    <div>
-                                        <p class="font-semibold">Angga Air</p>
-                                        <p class="text-sm text-garuda-grey mt-[2px]">08:30 - 12:00</p>
-                                    </div>
-                                    <div class="flex flex-col gap-[2px] items-center justify-center">
-                                        <p class="text-sm text-garuda-grey">12 hours</p>
-                                        <div class="flex items-center gap-[6px]">
-                                            <p class="font-semibold">CGK</p>
-                                            <img src="assets/images/icons/transit-black.svg" alt="icon">
-                                            <p class="font-semibold">HND</p>
-                                        </div>
-                                        <p class="text-sm text-garuda-grey">Transit 1x</p>
-                                    </div>
+                            </div>
+                            <a href="#" class="flex items-center rounded-[50px] py-3 px-5 gap-[10px] w-fit bg-garuda-black">
+                                <p class="font-semibold text-white">Details</p>
+                            </a>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex flex-col gap-[2px] items-center justify-center">
+                                <p class="text-sm text-garuda-grey">{{number_format($flight->segments->first()->time->diffInHours($flight->segments->last()->time), 0)}} Hours</p>
+                                <div class="flex items-center gap-[6px]">
+                                    <p class="font-semibold">{{$transaction->flight->segments->first()->airport->iata_code}}</p>
+                                    @if($transaction->flight->segments->count() > 2)
+                                    <img src="{{asset('assets/images/icons/transit-black.svg')}}" alt="icon">
+                                    @else
+                                    <img src="{{asset('assets/images/icons/direct-black.svg')}}" alt="icon">
+                                    @endif
+                                    <p class="font-semibold">{{$transaction->flight->segments->last()->airport->iata_code}}</p>
                                 </div>
+                                @if($transaction->flight->segments->count() > 2)
+                                <p class="text-sm text-garuda-grey">Transit {{$transaction->flight->segments->count() - 2}}x</p>
+                                @else
+                                <p class="text-sm text-garuda-grey">Direct</p>
+                                @endif
+                            </div>
+                            <p class="font-semibold text-garuda-green text-center">{{'Rp ' . number_format($transaction->flight->classes->first()->price, 0, ',', '.')}}</p>
+                        </div>
+                        <hr class="border-[#E8EFF7]">
+                            <div class="flex items-center rounded-[20px] gap-[14px]">
+                                <div class="flex w-[120px] h-[100px] shrink-0 rounded-[20px] overflow-hidden">
+                                    @if($transaction->class->class_type === 'economy')
+                                    <img src="{{asset('assets/images/thumbnails/economy-seat.png')}}"
+                                        class="w-full h-full object-cover" alt="icon">
+                                    @else
+                                    <img src="{{asset('assets/images/thumbnails/business-seat.png')}}"
+                                        class="w-full h-full object-cover" alt="icon">
+                                    @endif
+                                </div>
+                                <div>
+                                    <p class="font-bold text-xl leading-[30px]">{{Str::ucfirst($transaction->class->class_type)}} Class</p>
+                                    <p class="text-garuda-grey mt-1">Enjoy our good flight experience</p>
+                                </div>
+                            </div>
+                    </div>
                             </div>
                             <hr class="border-[#E8EFF7]">
                             <div class="flex items-center rounded-[20px] gap-[14px]">
@@ -90,17 +116,17 @@
                         <p class="font-semibold">Booking Transaction ID</p>
                         <div class="flex items-center rounded-full py-3 px-5 gap-[10px] bg-garuda-bg-grey">
                             <img src="assets/images/icons/note-favorite-black.svg" class="w-5 flex shrink-0" alt="icon">
-                            <p class="font-semibold">GARUDABWA19384923</p>
+                            <p class="font-semibold">{{$transaction->code}}</p>
                         </div>
                     </div>
                     <div class="flex flex-col gap-5">
                         <p class="font-semibold">Phone No.</p>
                         <div class="flex items-center rounded-full py-3 px-5 gap-[10px] bg-garuda-bg-grey">
                             <img src="assets/images/icons/call-black.svg" class="w-5 flex shrink-0" alt="icon">
-                            <p class="font-semibold">628123123123123</p>
+                            <p class="font-semibold">{{$transaction->phone}}</p>
                         </div>
                     </div>
-                    <a href="index.html"
+                    <a href="{{route('flight.index')}}"
                         class="w-full rounded-full py-3 px-5 text-center bg-garuda-blue hover:shadow-[0px_14px_30px_0px_#0068FF66] transition-all duration-300">
                         <span class="font-semibold text-white">Book More Tickets</span>
                     </a>
